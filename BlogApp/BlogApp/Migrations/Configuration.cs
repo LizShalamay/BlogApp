3969e609b4fce18a353daa1e035e58e3dca7e45b
LizShalamay <shalamay.lizzy@gmail.com>
2018-01-12 19:00:26 +0200

    Add method Seed in Configuration.cs

--- BlogApp/BlogApp/Migrations/Configuration.cs
+++ BlogApp/BlogApp/Migrations/Configuration.cs
@@ -1,9 +1,18 @@
 namespace BlogApp.Core.Migrations
 {
+    using BlogApp.Core.Identity;
+    using BlogApp.Core.Objects;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.AspNet.Identity;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using BlogApp.Controllers;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Collections.Generic;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BlogApp.Core.DataBase.BlogContext>
     {
@@ -14,10 +23,44 @@ namespace BlogApp.Core.Migrations
 
         protected override void Seed(BlogApp.Core.DataBase.BlogContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var userManager = new AppUserManager(new UserStore<User>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole { Name = "admin" },
+                new IdentityRole { Name = "user" }
+            };
+
+            foreach(var role in roles)
+            {
+                var currentRole = roleManager.FindByName(role.Name);
+                if(currentRole == null)
+                {
+                    roleManager.Create(role);
+                }
+            }
+
+            var admin = new User
+            {
+                UserName = "admin",
+                Login = "admin",
+                EmailConfirmed = true,
+                Id = Guid.NewGuid().ToString()
+            };
+            string adminPassword = "admin123";
+
+            var adminUser = userManager.FindByName(admin.UserName);
+            if(adminUser == null)
+            {
+                var result = userManager.Create(admin, adminPassword);
+                userManager.SetLockoutEnabled(admin.Id, false);
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(admin.Id, roles[0].Name);
+                }
+            }            
         }
     }
 }
